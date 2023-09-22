8d4340aed58a9127a6e654a92b5cd696cbb852d4
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-22 18:43:04 +0300

    added project 10

---
+++ 02.DataTypesAndVariables-Lab/10.LowerOrUpper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char letter = char.Parse(Console.ReadLine());
+
+        bool isUp = char.IsUpper(letter);
+
+        if (isUp == true)
+        {
+            Console.WriteLine("upper-case");
+        }
+        else if (isUp == false)
+        {
+            Console.WriteLine("lower-case");
+        }
+    }
+}
