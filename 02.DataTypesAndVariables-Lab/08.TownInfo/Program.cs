24a5a5c3ce1f77d6e598ca67e691c2bb37d8f0bc
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-22 18:12:18 +0300

    added 08 project

---
+++ 02.DataTypesAndVariables-Lab/08.TownInfo/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string townName = Console.ReadLine();
+        int population = int.Parse(Console.ReadLine());
+        double area = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
+    }
+}
