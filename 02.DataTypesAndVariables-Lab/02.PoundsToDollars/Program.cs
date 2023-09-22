98d4cd8a89931ba8ed341e796da11a730d6de430
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-22 11:24:16 +0300

    added a new projects

---
+++ 02.DataTypesAndVariables-Lab/02.PoundsToDollars/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double britishPound = double.Parse(Console.ReadLine());
+        double usdDollar = britishPound * 1.31;
+
+        Console.WriteLine($"{usdDollar:f3}");
+    }
+}
---
+++ 02.DataTypesAndVariables-Lab/03.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        double sum = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            double currentNumber = double.Parse(Console.ReadLine());
+            sum += currentNumber;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
