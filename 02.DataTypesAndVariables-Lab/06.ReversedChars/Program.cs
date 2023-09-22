02036bf89a2f59f10ab8dc2913e8979e80238233
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-22 17:50:58 +0300

    added 06 project

---
+++ 02.DataTypesAndVariables-Lab/06.ReversedChars/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char first = char.Parse(Console.ReadLine());
+        char second = char.Parse(Console.ReadLine());
+        char third = char.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{third} {second} {first}");
+    }
+}
