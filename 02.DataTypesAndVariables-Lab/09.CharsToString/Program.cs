3b2c9d78de9510b97a7082e720dbfb2486f30224
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-22 18:23:27 +0300

    added 09 project

---
+++ 02.DataTypesAndVariables-Lab/09.CharsToString/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char first = char.Parse(Console.ReadLine());
+        char second = char.Parse(Console.ReadLine());
+        char third = char.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{first}{second}{third}");
+
+    }
+}
