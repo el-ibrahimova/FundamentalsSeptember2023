50607f082ca36bb58640eed7320db6bcbeec3a8c
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-22 18:03:41 +0300

    added 07 project

---
+++ 02.DataTypesAndVariables-Lab/07.ConcatNames/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        string delimiter = Console.ReadLine();
+
+        string result = firstName+ delimiter + lastName;
+        Console.WriteLine(result);
+    }
+}
