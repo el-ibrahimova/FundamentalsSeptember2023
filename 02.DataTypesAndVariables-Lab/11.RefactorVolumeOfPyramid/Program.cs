6c186ad05358d1fc0275e9606018de88c736172b
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-22 19:09:32 +0300

    added 11 project

---
+++ 02.DataTypesAndVariables-Lab/11.RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double length, width, height = 0;
+        length = double.Parse(Console.ReadLine());
+        width = double.Parse(Console.ReadLine());
+        height = double.Parse(Console.ReadLine());
+               
+        Console.Write("Length: ");
+        Console.Write("Width: ");
+        Console.Write("Height: ");
+
+       double volume = (length * width * height) /3;
+
+        Console.Write($"Pyramid Volume: {volume:f2}");
+    }
+}
