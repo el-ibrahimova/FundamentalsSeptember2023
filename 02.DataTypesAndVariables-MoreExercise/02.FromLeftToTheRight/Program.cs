f10a13c843974d519b5053bb02bd078db2d8976c
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-24 23:36:42 +0300

    added 02 project

---
+++ 02.DataTypesAndVariables-MoreExercise/02.FromLeftToTheRight/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _02.FromLeftToTheRight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string num = Console.ReadLine();
+              
+                int indexSpace = num.IndexOf(' ');
+                string left = "";
+                string right = "";
+                long leftSum = 0, rightSum = 0;
+
+                for (int j = 0; j < num.IndexOf(' '); j++)
+                {
+                    left += num[j];
+                }
+
+                for (int k = num.IndexOf(' '); k < num.Length; k++)
+                {
+                    right += num[k];
+                }
+            
+                    leftSum = long.Parse(left);
+                    rightSum = long.Parse(right);
+
+                    long sum = 0;
+                    if (leftSum > rightSum)
+                    {
+                        while (leftSum > 0)
+                        {
+                            long lastDigit = leftSum % 10;
+                            sum += lastDigit;
+                            leftSum /= 10;
+                        }
+                        Console.WriteLine(sum);
+                    }
+                    else
+                    {
+                        while (rightSum > 0)
+                        {
+                            long lastDigit = rightSum % 10;
+                            sum += lastDigit;
+                            rightSum /= 10;
+                        }
+                        Console.WriteLine(sum);
+                    }
+                }
+
+            }
+         
+        }
+    }
