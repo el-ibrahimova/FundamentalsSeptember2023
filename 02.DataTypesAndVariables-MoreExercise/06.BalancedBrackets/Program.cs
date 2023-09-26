815807c346ca951072955b40f0d674b7c1f24dd3
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-26 19:40:16 +0300

    added project

---
+++ 02.DataTypesAndVariables-MoreExercise/06.BalancedBrackets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06.BalancedBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int openBracelet = 0;
+            int closeBracelet = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+               
+                 openBracelet = input.IndexOf('(');
+                 closeBracelet = input.IndexOf(')');
+
+                if (openBracelet == '(')
+                {
+                    openBracelet++;
+                }
+                else if (closeBracelet == ')')
+                { 
+                    closeBracelet++;
+                }
+            }
+
+            if (openBracelet == closeBracelet)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
