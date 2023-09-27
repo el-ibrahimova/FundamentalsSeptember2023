d49a286494b439d7c94e8be5b75a7e7ce7ef24d8
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-27 22:47:16 +0300

    added project

---
+++ 03.Arrays-ListsBasics/01.DayOfWeek/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[7] {         
+                   "Monday",        // 0
+                    "Tuesday",      // 1
+                    "Wednesday",    //2
+                    "Thursday",     //3
+                    "Friday",       //4
+                    "Saturday",     //5
+                    "Sunday"        //6
+            };
+
+            int number = int.Parse(Console.ReadLine());
+            if (number < 1 || number > 7)
+            {
+                Console.WriteLine("Invalid Day!");
+            }
+            else
+            {
+                Console.WriteLine(days[number-1]);
+            }
+        }
+    }
+}
