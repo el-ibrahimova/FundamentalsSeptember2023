678531cefe3fcf3b5ab9d9e5287f615dd5815cb8
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-28 23:19:56 +0300

    add

--- 03.Arrays-ListsBasics/03.RoundingNumbers/Program.cs
+++ 03.Arrays-ListsBasics/03.RoundingNumbers/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double[] nums = Console.ReadLine()
+                             .Split()
+                             .Select(double.Parse)
+                             .ToArray();
+
+            int[] roundedNums = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                roundedNums[i] = (int)Math.Round(nums[i], MidpointRounding.AwayFromZero);
+            Console.WriteLine($"{nums[i]} => {roundedNums[i]}");
+            }
+
         }
     }
 }
--- 03.Arrays-ListsBasics/04.ReverseArrayOfStrings/Program.cs
+++ 03.Arrays-ListsBasics/04.ReverseArrayOfStrings/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] text = Console.ReadLine()
+               .Split()
+               .ToArray();
+
+            for (int i = 0; i < text.Length / 2; i++)
+            {
+                string firstElement = text[i];
+                string lastElement = text[text.Length - 1 - i];
+
+                text[i] = lastElement;
+                text[text.Length - 1 - i] = firstElement;
+            }
+
+            Console.WriteLine(string.Join(" ", text));
         }
     }
 }
