0d666968efb02ee82e71644a93896a2a87023554
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-29 20:23:54 +0300

    edit code

--- 03.Arrays-ListsBasics/05.SumEvenNumbers/Program.cs
+++ 03.Arrays-ListsBasics/05.SumEvenNumbers/Program.cs
@@ -11,6 +11,17 @@
 
             int sum = 0;
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    sum += currentNumber;
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
