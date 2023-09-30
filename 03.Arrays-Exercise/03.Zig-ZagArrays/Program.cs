606bb80da8c89b49be9e678c8e2f738c8658eac0
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-30 13:12:34 +0300

    add project 03

--- 03.Arrays-Exercise/03.Zig-ZagArrays/Program.cs
+++ 03.Arrays-Exercise/03.Zig-ZagArrays/Program.cs
@@ -6,6 +6,8 @@
         {
             // 4
             int n = int.Parse(Console.ReadLine());
+            bool isFirstArrSelected = true;
+
             string[] firstArr = new string[n];
             string[] secondArr = new string[n];
 
@@ -14,8 +16,18 @@
                 string numbers = Console.ReadLine();       // "1 5" 
                 string[] numbersArray = numbers.Split();   // ["1", "5"] 
 
-                firstArr[i] = numbersArray[0];
-                secondArr[i] = numbersArray[1];
+                if (isFirstArrSelected)
+                {
+                    firstArr[i] = numbersArray[0];
+                    secondArr[i] = numbersArray[1];
+                }
+                else 
+                {
+                    firstArr[i] = numbersArray[1];
+                    secondArr[i] = numbersArray[0];
+                }
+
+                isFirstArrSelected = !isFirstArrSelected;
             }
 
             Console.WriteLine(string.Join(" ",firstArr));
