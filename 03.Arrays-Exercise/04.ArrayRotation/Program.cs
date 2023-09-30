a7f4299fbe2612fdf8dd77abd77df12f46743311
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-09-30 17:55:51 +0300

    added projects

---
+++ 03.Arrays-Exercise/04.ArrayRotation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _04.ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+ 
+            for (int i = 0; i < n; i++)
+            {
+                string firstElement = array[0];
+
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+
+                array[array.Length - 1] = firstElement;
+            }
+
+            Console.WriteLine(string.Join(" ",array));
+        }
+    }
+}
