049f8b758d3f6c2288567293740603c3495c199a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-01 12:37:12 +0300

    create project 06

---
+++ 03.Arrays-Exercise/06.EqualSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Serialization;
+
+namespace _06.EqualSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += array[j];
+                }
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    rightSum += array[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;                  
+                }
+
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
