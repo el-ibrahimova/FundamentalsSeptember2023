ac88fb925b6d77bade68d478a3aa9573428409ce
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-01 13:06:15 +0300

    create project 08

---
+++ 03.Arrays-Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08.MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+            
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+ 1; j < array.Length; j++)
+                {
+
+                    if (array[i] + array[j] == sum)
+                    {
+                        Console.WriteLine($"{array[i]} {array[j]}");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
