3e90437d8113929a276584c495fe5721b5cb07ad
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-01 14:11:47 +0300

    create project 09

--- 03.Arrays-Exercise/08.MagicSum/Program.cs
+++ 03.Arrays-Exercise/08.MagicSum/Program.cs
@@ -13,7 +13,7 @@
 
             for (int i = 0; i < array.Length; i++)
             {
-                for (int j = i+ 1; j < array.Length; j++)
+                for (int j = i + 1; j < array.Length; j++)
                 {
 
                     if (array[i] + array[j] == sum)
@@ -22,7 +22,6 @@
                     }
                 }
             }
-
 
         }
     }
