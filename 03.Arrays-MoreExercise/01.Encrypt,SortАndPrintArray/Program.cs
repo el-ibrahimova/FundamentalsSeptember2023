61c988d6e8a1204b1dca296848ccc3268750b58a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-01 19:16:16 +0300

    added new project

---
+++ 03.Arrays-MoreExercise/01.Encrypt,SortАndPrintArray/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01.Encrypt_SortАndPrintArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] names = new string[n];
+            int[] currentNameValue = new int[n]; 
+            int sum = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                names[i] = Console.ReadLine();
+                string currentName = names[i];
+
+                sum = 0;
+
+                for (int j = 0; j < currentName.Length; j++)
+                {
+                    char digit = currentName[j];
+                    int value = (int)digit;
+
+                     if (digit == 'a' || digit == 'i' || digit == 'o' || digit == 'u' || digit == 'e'
+                      || digit == 'A' || digit == 'I' || digit == 'O' || digit == 'U' || digit == 'E')
+                        {
+                            sum += value * currentName.Length;
+                        }
+                        else
+                        {
+                            sum += value / currentName.Length;
+                        }
+                }
+                currentNameValue[i] = sum;
+
+            }
+            Array.Sort(currentNameValue);
+            for (int i = 0; i < currentNameValue.Length; i++)
+            {
+                Console.WriteLine(currentNameValue[i]);
+            }
+
+        }
+    }
+}
+              
