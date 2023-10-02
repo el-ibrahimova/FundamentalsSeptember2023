29a9e1ce7dd4041f8a4565ea0901bb6f674599f3
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-02 09:30:14 +0300

    create project 02

---
+++ 03.Arrays-MoreExercise/02.PascalTriangle/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lines = int.Parse(Console.ReadLine());
+            long[] row = new long[lines];
+            long[] current = new long[lines];
+           
+            row[0] = 1; //Startup row is always equal to "1"
+            Console.WriteLine(row[0]);
+          
+            for (int r = 1; r < lines; r++)
+            {
+                current[0] = 1; //Every 1st value in the row is always equal to "1"
+                Console.Write($"{current[0]} ");
+               
+                for (int c = 1; c <= r; c++)
+                {
+                    current[c] = row[c - 1] + row[c];
+                    Console.Write($"{current[c]} ");
+                }
+                for (int j = 0; j < lines; j++)
+                {
+                    row[j] = current[j];
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
