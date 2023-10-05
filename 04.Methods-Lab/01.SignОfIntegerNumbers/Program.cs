d3a8ad9ff1bfa6df56bdbda13f433e9fb21f45e9
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-05 21:04:40 +0300

    Update

--- 04.Methods-Lab/01.SignОfIntegerNumbers/Program.cs
+++ 04.Methods-Lab/01.SignОfIntegerNumbers/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintNumberSign(n);
+        }
+
+        private static void PrintNumberSign(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else // zero
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
         }
     }
 }
