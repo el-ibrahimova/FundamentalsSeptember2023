c597655e43c29b66546f119657884d99d7002439
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-05 23:46:58 +0300

    create project

--- 04.Methods-Lab/04.PrintingTriangle/Program.cs
+++ 04.Methods-Lab/04.PrintingTriangle/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintTriangle(n);
+        }
+
+        static void PrintTriangle(int n)
+        {
+            PrintTopPart(n);
+
+            PrintBottomPart(n);
+        }
+
+        static void PrintBottomPart(int n)
+        {
+            for (int row = n - 1; row >= 1; row--)
+            {
+                PrintRow(row);
+            }
+        }
+        static void PrintTopPart(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                PrintRow(row);
+            }
+        }
+
+        static void PrintRow(int row)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write(col+" ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
---
+++ 04.Methods-Lab/05.Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            TotalPriceOfProduct(product, quantity);
+        }
+
+       static void TotalPriceOfProduct(string product, int quantity)
+        {
+            double totalPrice=0;
+            switch (product)
+            {
+                case "coffee":
+                    totalPrice = quantity * 1.5;
+                    break;
+                case "water":
+                    totalPrice = quantity * 1;
+                    break;
+                case "coke":
+                    totalPrice = quantity * 1.4;
+                    break;
+                case "snacks":
+                    totalPrice = quantity * 2;
+                    break;
+            }
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
