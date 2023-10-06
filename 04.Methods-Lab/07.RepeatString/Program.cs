5f172ddcd60a5a23f1286ccbb419e7ee6e9b5b17
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-06 21:09:09 +0300

    create project

---
+++ 04.Methods-Lab/07.RepeatString/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string result = RepeatString(text, n);
+                Console.WriteLine(result);
+            
+        }
+
+        static string RepeatString(string text, int repeat)
+        {
+            string newText = "";
+            for (int i = 0; i < repeat; i++)
+            {
+                newText += text;
+            }
+            return newText;
+        }
+    }
+}
---
+++ 04.Methods-Lab/08.MathPower/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            int result = GetMathPower(number, power);
+            Console.WriteLine(result);
+
+
+        }
+
+         static int GetMathPower(int number, int power)
+        {
+            int result = (int)Math.Pow(number, power);
+            return result;
+            
+        }
+    }
+}
