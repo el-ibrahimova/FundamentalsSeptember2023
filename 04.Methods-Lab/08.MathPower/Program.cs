746c88f78d1d8ad12ff8468d63114aeefa0db4b4
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-06 23:24:58 +0300

    create project

--- 04.Methods-Lab/08.MathPower/Program.cs
+++ 04.Methods-Lab/08.MathPower/Program.cs
@@ -4,20 +4,21 @@
     {
         static void Main(string[] args)
         {
-            int number = int.Parse(Console.ReadLine());
+            double number = double.Parse(Console.ReadLine());
             int power = int.Parse(Console.ReadLine());
 
-            int result = GetMathPower(number, power);
+            double result = GetMathPower(number, power);
             Console.WriteLine(result);
-
-
         }
 
-         static int GetMathPower(int number, int power)
+         static double GetMathPower(double number, int power)
         {
-            int result = (int)Math.Pow(number, power);
-            return result;
-            
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            return result;            
         }
     }
 }
---
+++ 04.Methods-Lab/09.GreaterOfTwoValues/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _09.GreaterOfTwoValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "string")
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                string greaterValue = GetMax(a, b);
+                Console.WriteLine(greaterValue);
+             
+            }
+            else if (type == "int")
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int greaterValue = GetMax(a, b);
+                Console.WriteLine(greaterValue);
+            }
+            else if (type == "char")
+            {
+                char a = char.Parse(Console.ReadLine());
+                char b = char.Parse(Console.ReadLine());
+                char greaterValue = GetMax(a, b);
+                Console.WriteLine(greaterValue);
+            }
+
+        }
+
+        static int GetMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            return b;
+        }
+
+      static char GetMax(char a, char b)
+        {
+            if (a > b)  //'a' > 'b' => 95 > 96
+                {
+                return a;
+            }
+            return b;
+        }
+        
+        static string GetMax(string a, string b)
+        {
+            // "Stoyan" > "Pesho"
+            int result = a.CompareTo(b);
+            // a == b => 0
+            // a > b => 1
+            // a < b => -1
+
+            if (result > 0) // a>0
+            {
+                return a;
+            }
+            return b;
+        }
+    }
+}
