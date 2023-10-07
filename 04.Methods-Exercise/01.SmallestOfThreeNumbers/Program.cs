5479190ddd990af8ef57636dfec25bfe365a089d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-07 11:53:43 +0300

    create project

--- 04.Methods-Exercise/01.SmallestOfThreeNumbers/Program.cs
+++ 04.Methods-Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int smallestNumber = SmallestNumber(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(smallestNumber);
+
+        }
+
+        static int SmallestNumber(int first, int second, int third)
+        {
+            int max;
+            int min;
+            int mid;
+
+            if (first > second)
+            {
+                mid = first;
+                min = second;
+            }
+            else
+            {
+                mid = second;
+                min = first;
+            }
+
+            if (mid > third)
+            {
+                max = mid;
+                if (min > third)
+                {
+                    mid = min;
+                    min = third;
+                }
+                else
+                {
+                    mid = third;
+                }
+            }
+            else max = third;
+
+
+            return min;
         }
     }
 }
