8fd6580b2f92f014c4fde6b1b22d214cd0c1e7a2
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-07 22:11:53 +0300

    create project

--- 04.Methods-Exercise/08.FactorialDivision/Program.cs
+++ 04.Methods-Exercise/08.FactorialDivision/Program.cs
@@ -6,21 +6,21 @@
         {
             int num1 = int.Parse(Console.ReadLine());
             int num2 = int.Parse(Console.ReadLine());
-            int factorial1 = FactorialNum1(num1);
-            int factorial2 = FactorialNum2(num2);
+            double factorial1 = FactorialNum1(num1);
+            double factorial2 = FactorialNum2(num2);
 
             PrintResult(factorial1, factorial2);
         }
 
-        private static void PrintResult(int factorial1, int factorial2)
+        private static void PrintResult(double factorial1, double factorial2)
         {
             double result = (double)factorial1 / factorial2;
             Console.WriteLine($"{result:f2}");
         }
 
-        private static int FactorialNum1(int num1)
+        private static double FactorialNum1(int num1)
         {
-            int factorial = 1;
+           double factorial = 1;
             for (int i = 0; i < num1; i++)
             {
                 factorial *= (num1 - i);
@@ -28,9 +28,9 @@
             return factorial;
         }
 
-        private static int FactorialNum2(int num2)
+        private static double FactorialNum2(int num2)
         {
-            int factorial = 1;
+            double factorial = 1;
             for (int i = 0; i < num2; i++)
             {
                 factorial *= (num2 - i);
---
+++ 04.Methods-Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                bool isPalindromNumber = IsPalindrom(input);
+
+                if (!IsPalindrom(input))
+                {
+                    isPalindromNumber = false;
+                    Console.WriteLine("false");
+                } 
+                else if (IsPalindrom(input))
+                {
+                    isPalindromNumber = true;
+                    Console.WriteLine("true");
+                }
+               
+            }
+        }
+
+        static bool IsPalindrom(string symbol)
+        {
+            bool isPalindrom = true;
+            for (int i = 0; i < symbol.Length; i++)
+            {
+                if (symbol[i] == symbol[symbol.Length - 1 - i])
+                {
+                    isPalindrom = true;
+                    return true;
+                }
+                else
+                {
+                    isPalindrom = false;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
