f2ec4f358afe64d14eb259cb4d32116c6a54e4ab
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-07 15:54:14 +0300

    create project

---
+++ 04.Methods-Exercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _05.AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            SumSubstractsThird(first, second, third);
+            int result = SumSubstractsThird(first, second, third);
+            Console.WriteLine(result);
+        }
+
+        static int SumSubstractsThird(int first, int second, int third)
+        {
+            int sum = SumFirstAndSecond(first, second);
+            return sum - third;
+        }
+
+        static int SumFirstAndSecond(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
