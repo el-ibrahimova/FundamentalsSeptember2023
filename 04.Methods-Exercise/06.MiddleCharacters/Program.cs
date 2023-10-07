5b0ec676bf73f339fcc2a22e7d1bf638fdaa7720
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-07 16:24:58 +0300

    create project

---
+++ 04.Methods-Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            MiddleCharacters(text);
+        }
+
+        static void MiddleCharacters(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text.Length % 2 == 0)
+                {
+                    char symbol1 = text[text.Length / 2 - 1];
+                    char symbol2 = text[text.Length / 2];
+                    Console.WriteLine($"{symbol1}{symbol2}");
+                    break;
+                }
+                else
+                {
+                    char symbol = text[text.Length / 2];
+                    Console.WriteLine(symbol);
+                    break;
+                }
+            }
+        }
+    }
+}
