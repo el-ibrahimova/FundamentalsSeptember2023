de5aefcaab2678479bcfa62645d3d6a3dfba1394
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-07 12:03:58 +0300

    create project

--- 04.Methods-Exercise/02.VowelsCount/Program.cs
+++ 04.Methods-Exercise/02.VowelsCount/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = Console.ReadLine();
+
+            int sum = SumOfVouels(text);
+            Console.WriteLine(sum);
+
+        }
+
+        static int SumOfVouels(string text)
+        {
+            int vouels = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == 'a' || text[i] == 'o' || text[i] == 'u' || text[i] == 'e' || text[i] == 'i' ||
+                    text[i] == 'A' || text[i] == 'O' || text[i] == 'U' || text[i] == 'E' || text[i] == 'I')
+                {
+                    vouels += 1;
+                }
+            }
+            return vouels;
         }
     }
 }
