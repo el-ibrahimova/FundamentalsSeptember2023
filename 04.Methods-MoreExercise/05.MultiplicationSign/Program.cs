67c56508f5232d1ac3e31a65e882be699954309b
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-08 22:47:20 +0300

    update

---
+++ 04.Methods-MoreExercise/05.MultiplicationSign/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _05.MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            PositiveOrNegative(num1, num2, num3);
+        }
+
+       static void PositiveOrNegative(int num1, int num2, int num3)
+        {
+            Zero(num1, num2, num3);
+            Positive(num1, num2, num3);
+            Negative(num1, num2, num3);
+        }
+
+        static void Negative(int num1, int num2, int num3)
+        {
+            if ((num1 > 0 && num2 > 0 && num3 < 0) || (num1 > 0 && num2 < 0 && num3 > 0)
+                               || (num1 < 0 && num2 > 0 && num3 < 0) || (num1 < 0 && num2 < 0 && num3 < 0))
+            {
+                Console.WriteLine("negative");
+            }
+        }
+
+        static void Positive(int num1, int num2, int num3)
+        {
+            if ((num1 > 0 && num2 > 0 && num3 > 0) || (num1 > 0 && num2 < 0 && num3 < 0)
+                              || (num1 < 0 && num2 > 0 && num3 < 0) || (num1 < 0 && num2 < 0 && num3 > 0))
+            {
+                Console.WriteLine("positive");
+            }
+        }
+
+        static void Zero(int num1, int num2, int num3)
+        {
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+            {
+                Console.WriteLine("zero");
+            }
+        }
+    }
+}
