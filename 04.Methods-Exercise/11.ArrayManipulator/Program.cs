0728ee16efde21fc75d26b587b9890e2a00c5d30
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-08 12:53:48 +0300

    update

--- 04.Methods-Exercise/11.ArrayManipulator/Program.cs
+++ 04.Methods-Exercise/11.ArrayManipulator/Program.cs
@@ -45,7 +45,7 @@ namespace _11.ArrayManipulator
                             // пример: exchange 1
                             int index = int.Parse(arguments[1]);
                             // разделяме масива и разменяме местата според подадения индекс
-                            Exchange(numbers, index); // създаваме метод, в който ще изпълняваме командата за смяна на индексите
+                           numbers = Exchange(numbers, index); // създаваме метод, в който ще изпълняваме командата за смяна на индексите
                             break;
 
                         case "max":
@@ -86,16 +86,74 @@ namespace _11.ArrayManipulator
 
         }
 
-        static void Exchange(int[] numbers, int index)
+        static int [] Exchange(int[] numbers, int index)
         {
 
+            if (CheckForOutOfBound(numbers, index)) //масив, в който проверяваме дали индекса не е извън от границите
+            {
+                Console.WriteLine("Invalid index");
+                return numbers; // връщаме същия масив, защото метода ни е от тип масив
+            }
+
+
+            int[] changedArray = new int[numbers.Length];
+
+            //  0  1  2  3  4 - индекси
+            // [1, 3, 5, 7, 9]
+            // [0, 0, 0, 0, 0]   => [0, 0, 0, 1, 3]  => [5, 7, 9, 1, 3]
+
+            int changedArrayIndex = 0;
+            for (int i = index +1; i < numbers.Length; i++)
+            {
+                // int i = index +1 => по условие се вземат индексите след дадения
+                changedArray[changedArrayIndex] = numbers[i];
+                changedArrayIndex++;
+            }
+            // след този цикъл масива има вида changedArray [5, 7, 9, 0, 0] при подаден индекс 2
+
+            for (int i = 0; i < index; i++)
+            {
+                changedArray[changedArrayIndex] = numbers[i];
+                changedArrayIndex++;
+            }
+
+            // => numbers = [1,3,5,7,9]
+            // => changedArray = [5, 7, 9, 1, 3]
+            // за да има numbers[] новото съдържание правим следното:
+            numbers = changedArray;
+            // до тук новия масив съществува само в този метод.
+            // За да го подменим в цялата програма правим следното:
+            // => връщаме масив с return и променяме типа на масива от void на int []
+            return changedArray; 
         }
 
         static void PrintMaxNumber(int[] numbers, string type)
         // type = "even" || "odd"
         {
+            int maxIndex = -1; // границите на масива са от 0 до numbers.Length-1. За да сме сигурни, че индекса е Min => -1
+            int maxNumber = 0;
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (IsOddOrEven(numbers, type, i))
+                {
+                    if (numbers[i] >= maxNumber)
+                    {
+                        maxNumber = numbers[i];
+                        maxIndex = i;
+                    }
+                }
+            }
+            PrintIndex(maxIndex);
         }
+
+        private static bool IsOddOrEven(int[] numbers, string type, int i)
+        {
+            return (type == "odd" && numbers[i] % 2 != 0) ||
+                                (type == "even" && numbers[i] % 2 == 0);
+        }
+
+
         static void PrintMinNumber(int[] numbers, string type)
         // type = "even" || "odd"
         {
@@ -112,12 +170,29 @@ namespace _11.ArrayManipulator
             
         }
 
-        
+        static bool CheckForOutOfBound(int[] numbers, int index)
+        {
+            return index < 0 || index >= numbers.Length;
+        }
 
-        
+        static void PrintIndex(int maxIndex)
+        {
+            if (maxIndex != -1) // не е намерен мах индекс =>остава си първоначалната ст-ст
+            {
+                 Console.WriteLine(maxIndex);
+            }
+            else
+            {
+               Console.WriteLine("No matches");
+            }
+        }
 
-        
 
-        
+
+
+
+
+
+
     }
 }
