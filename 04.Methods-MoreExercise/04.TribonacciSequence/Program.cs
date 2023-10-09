e40637d076856a5b47edf647dfad8254576af339
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-09 12:42:45 +0300

    update

---
+++ 04.Methods-MoreExercise/04.TribonacciSequence/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04.TribonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+        }
+    }
+}
--- 04.Methods-MoreExercise/05.MultiplicationSign/Program.cs
+++ 04.Methods-MoreExercise/05.MultiplicationSign/Program.cs
@@ -20,7 +20,7 @@
         static void Negative(int num1, int num2, int num3)
         {
             if ((num1 > 0 && num2 > 0 && num3 < 0) || (num1 > 0 && num2 < 0 && num3 > 0)
-                               || (num1 < 0 && num2 > 0 && num3 < 0) || (num1 < 0 && num2 < 0 && num3 < 0))
+                               || (num1 < 0 && num2 > 0 && num3 > 0) || (num1 < 0 && num2 < 0 && num3 < 0))
             {
                 Console.WriteLine("negative");
             }
