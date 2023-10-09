2309c52c274f6a55ea8e3e4dfbe2191cad9b2265
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-09 15:08:55 +0300

    create project

---
+++ 04.Methods-MoreExercise/02.CenterPoint/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _02.CenterPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                double x1 = double.Parse(Console.ReadLine());
+                double y1 = double.Parse(Console.ReadLine());
+                double x2 = double.Parse(Console.ReadLine());
+                double y2 = double.Parse(Console.ReadLine());
+
+                ClosestPoint(x1, y1, x2, y2);
+        }
+
+            static void ClosestPoint(double x1, double y1, double x2, double y2)
+            {
+                double first = Math.Sqrt(Math.Pow(y1, 2) + Math.Pow(x1, 2));
+                double second = Math.Sqrt(Math.Pow(y2, 2) + Math.Pow(x2, 2));
+
+                if (first < second)
+                {
+                    Console.WriteLine($"({x1}, {y1})");
+                }
+                else if (second < first || second == first)
+                {
+                    Console.WriteLine($"({x2}, {y2})");
+                }
+            }
+        }
+    }
---
+++ 04.Methods-MoreExercise/03.LongerLine/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _03.LongerLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstX1 = double.Parse(Console.ReadLine());
+            double firstY1 = double.Parse(Console.ReadLine());
+            double firstX2 = double.Parse(Console.ReadLine());
+            double firstY2 = double.Parse(Console.ReadLine());
+
+            double secondX1 = double.Parse(Console.ReadLine());
+            double secondY1 = double.Parse(Console.ReadLine());
+            double secondX2 = double.Parse(Console.ReadLine());
+            double secondY2 = double.Parse(Console.ReadLine());
+
+
+        //    (x1 - x2) ^ 2 + (y1 - y2) ^ 2
+
+            double first = Math.Sqrt(Math.Pow(firstY1-firstY2, 2) + Math.Pow(firstX1-firstX2, 2));
+            double second = Math.Sqrt(Math.Pow(secondY1 -secondY2, 2) + Math.Pow(secondX1-secondX2, 2));
+
+            if (first > second)
+            {
+                Console.WriteLine($"({firstX2}, {firstY2})({firstX1}, {firstY1})");
+            }
+            else if (second > first)
+            {
+                Console.WriteLine($"({secondX2}, {secondY2})({secondX1}, {secondY1})");
+            }
+        }
+    }
+    
+}
