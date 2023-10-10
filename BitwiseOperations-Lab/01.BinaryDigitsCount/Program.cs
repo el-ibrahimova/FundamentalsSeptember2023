b8306cfb70bbb20ca8feaf97f1cc8b4d03445247
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-10 23:29:36 +0300

    create project

--- BitwiseOperations-Lab/01.BinaryDigitsCount/Program.cs
+++ BitwiseOperations-Lab/01.BinaryDigitsCount/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace _01.BinaryDigitsCount
+﻿/*
+ * You are given a positive integer number and one binary digit B (0 or 1). Your task is to write a program that finds the number of binary digits (B) in a given integer.
+*/
+
+namespace _01.BinaryDigitsCount
 {
     internal class Program
     {
---
+++ BitwiseOperations-Lab/02.BitAtPosition1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Write a program that prints the bit at position 1 of the given integer. 
+We use the standard counting: from right to left, starting from 0.
+*/
+
+namespace _02.BitAtPosition1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int mask = 1 << 1;//after shift left with 1 we have 1 value in mask at position 1 another are 0
+
+            int result = (number & mask) >> 1;//after shift right with 1 taken last value(0 or 1) because another are 0 using &
+            Console.WriteLine(result);
+
+        }
+    }
+}
