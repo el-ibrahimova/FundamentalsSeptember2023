6f8c4c87763e77b4a898beeaf1e23d42625c0ca1
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-12 22:53:46 +0300

    update

--- 05.Lists-Lab/06.ListManipulationBasics/Program.cs
+++ 05.Lists-Lab/06.ListManipulationBasics/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToList();
+
+            string line;
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string[] lineTokens = line.Split().ToArray();
+                string command = lineTokens[0];
+                int number = int.Parse(lineTokens[1]); 
+                
+                
+                if (command == "Add") // на първи индекс стои числото, което трябва да добавим = Add 3
+                {  
+                     numbers.Add(number);
+                }
+                else if (command == "Insert") // Insert 8 3
+                {  
+                    int index = int.Parse(lineTokens[2]);
+                    numbers.Insert(index, number);
+                }
+                else if (command == "Remove")  // Remove 2
+                {
+                    numbers.Remove(number);
+                }
+                else if (command == "RemoveAt")  // RemoveAt 1
+                {
+                    int index = number;
+                    numbers.RemoveAt(index);
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
+    
 }
