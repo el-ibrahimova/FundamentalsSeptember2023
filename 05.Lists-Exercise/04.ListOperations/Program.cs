b73458cc66b87d00206d0da9816e1f568ac7353d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-14 11:42:27 +0300

    update

--- 05.Lists-Exercise/04.ListOperations/Program.cs
+++ 05.Lists-Exercise/04.ListOperations/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = Console.ReadLine()
+                  .Split()
+                  .Select(int.Parse)
+                  .ToList();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] arguments = input.Split();
+
+
+                if (arguments[0] == "Add")
+                {
+                    numbers.Add(arguments[1]);
+                }
+                else if (arguments[0] == "Insert")
+                {
+                }
+                else if (arguments[0] == "Remove")
+                {
+
+                }
+                else if (arguments[0] == "Shift")
+                {
+                    if (arguments[1] == "left")
+                    {
+
+                    }
+                    else if (arguments[1] == "right")
+                    { 
+                    
+                    }
+                }
+            
+            }
+
         }
     }
 }
