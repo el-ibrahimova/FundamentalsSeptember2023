b979beaf320e0ea552319adbfad740c007d7dd95
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-14 18:54:49 +0300

    create project

---
+++ 05.Lists-MoreExercises/01.Messaging/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _01.Messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+                List<int> numbers = Console.ReadLine().
+                    Split().
+                    Select(int.Parse).
+                    ToList();
+
+                List<char> text = Console.ReadLine().ToList();
+
+                List<char> letters = new List<char>();
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    int currentIndex = GetIndex(numbers[i], text.Count);
+                    letters.Add(text[currentIndex]); // прибавяме буквите в новия лист
+                    text.RemoveAt(currentIndex);
+                }
+                Console.WriteLine(string.Join("", letters));
+            }
+            static int CalculateSum(int currentNumber) 
+            // изчисляваме сумата на всеки елемент от листа => от тук получваме кой индекс да вземем от листа text
+            {
+                int sum = 0;
+                while (currentNumber > 0)
+                {
+                    int lastDigit = currentNumber % 10;
+                    sum += lastDigit;
+                    currentNumber /= 10;
+                }
+                return sum;
+            }
+            static int GetIndex(int currentNumber, int count)
+            // с този метод намираме на кой индекс( и как да го валидираме, за да не е извън границите) е буквата от новия ни лист
+            {
+                int index = CalculateSum(currentNumber);
+
+                if (index > count)
+                {
+                    if (index % count == 0)
+                    {
+                        index = count - 1;
+                    }
+                    else
+                    {
+                        index %= count;
+                    }
+                }
+                return index;
+            }
+        }
+    }
+
+  
---
+++ 05.Lists-MoreExercises/02.CarRace/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _02.CarRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            double timeLeft = 0;
+            double timeRight = 0;
+            int indexFinish = numbers.Count / 2;
+
+                        
+            for (int i = 0; i < indexFinish; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    timeLeft = timeLeft * 0.8;
+                }
+                else 
+                {
+                    timeLeft += numbers[i];
+                }
+            }
+
+
+            for (int i = numbers.Count - 1; i > indexFinish; i--)
+            {
+                if (numbers[i] == 0)
+                {
+                    timeRight = timeRight * 0.8;
+                }
+                else
+                {
+                    timeRight += numbers[i];
+                }
+            }
+
+            if (timeLeft < timeRight)
+            {
+                Console.WriteLine($"The winner is left with total time: {timeLeft.ToString():f1}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {timeRight.ToString():f1}");
+            }
+
+        }
+    }
+}
