551ec66951c3ba560eec39fb1ce1cbb95f12f270
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-15 12:32:32 +0300

    create project

---
+++ MidExamPreparation/ComputerStore/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ComputerStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double totalPriceWithTaxes = 0;
+            double totalPrice = 0;
+            double taxes = 0;
+
+            while (input != "special" && input != "regular")
+            {
+                double price = double.Parse(input);
+
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                else if (price == 0)
+                {
+                    Console.WriteLine("Invalid order!");
+                }
+                else
+                {
+                    totalPrice += price;
+                    taxes = totalPrice * 0.2;
+                    totalPriceWithTaxes = totalPrice + taxes;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (input == "special")
+            {
+                totalPriceWithTaxes = totalPriceWithTaxes * 0.9;
+            }
+
+            Console.WriteLine($"Congratulations you've just bought a new computer!\r\nPrice without taxes: {totalPrice:f2}$\r\nTaxes: {taxes:f2}$\r\n-----------\r\nTotal price: {totalPriceWithTaxes:f2}$");
+
+        }
+    }
+}
---
+++ MidExamPreparation/SoftUniReception/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SoftUniReception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int secondEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int thirdEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int studentCount = int.Parse(Console.ReadLine());
+
+            int timeNeeded = 0;
+            int maxStudentForHour = firstEmployeeEfficiency + secondEmployeeEfficiency + thirdEmployeeEfficiency;
+
+
+            if (studentCount % maxStudentForHour == 0)
+            {
+                timeNeeded = studentCount / maxStudentForHour;
+            }
+            else if (studentCount % maxStudentForHour != 0)
+            {
+                timeNeeded = studentCount / maxStudentForHour + 1;
+            }
+
+            if (timeNeeded > 3 && timeNeeded % 3 == 0)
+            {
+                timeNeeded += timeNeeded / 3 - 1;
+            }
+            else if (timeNeeded > 3)
+            {
+                timeNeeded += timeNeeded / 3;
+            }
+
+            Console.WriteLine($"Time needed: {timeNeeded}h.");
+        }
+    }
+}
