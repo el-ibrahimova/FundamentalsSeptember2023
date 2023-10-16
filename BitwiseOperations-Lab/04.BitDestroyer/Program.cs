91d90851c04dc62da7d0db2588bd38a7804aeea6
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-16 10:07:56 +0300

    create project

---
+++ BitwiseOperations-Lab/04.BitDestroyer/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.BitDestroyer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+
+           
+            // трябва да променим числото на позиция пosition  да стане 0
+
+            // преместваме position пъти числото 1 до нужната позиция
+             
+            // 010100100001 = number
+            // 000000100000 = mask
+            // 111111011111 = reversed
+            
+            // 010100000001 = result
+            
+            
+            
+            int mask = 1 << position;
+            int reversed = ~mask;
+            int result = number & reversed;
+            Console.WriteLine(result);
+        }
+    }
+}
