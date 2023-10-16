919a4419ce8e025a9c511ac1696a8b96e3971cd8
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-16 17:21:00 +0300

    create project

---
+++ MidExamPreparation/ShoppingList/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace ShoppingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> initialList = Console.ReadLine().Split('!').ToList();
+
+            List<string> command = Console.ReadLine().Split().ToList();
+            string input = command[0];
+
+
+            while (input != "Go Shopping!")
+            {
+
+                if (command[0] == "Go")
+                {
+                    break;
+                }
+                else if (command[0] == "Urgent")
+                {
+                    string item = command[1];
+                    if (!initialList.Contains(item))
+                    {
+
+                        initialList.Insert(0, item);
+                    }
+                }
+                else if (command[0] == "Unnecessary")
+                {
+                    string item = command[1];
+                    if (initialList.Contains(item))
+                    {
+                        initialList.Remove(item);
+                    }
+                }
+                else if (command[0] == "Correct")
+                {
+                    string oldItem = command[1];
+                    string newItem = command[2];
+                    if (initialList.Contains(oldItem))
+                    {
+                        int indexOfOldItem = initialList.IndexOf(oldItem);
+                        initialList.Remove(oldItem);
+                        initialList.Insert(indexOfOldItem, newItem);
+                    }
+                }
+                else if (command[0] == "Rearrange")
+                {
+                    string item = command[1];
+                    if (initialList.Contains(item))
+                    {
+                        initialList.Remove(item);
+                        initialList.Add(item);
+                    }
+                }
+
+                command = Console.ReadLine().Split().ToList();
+                input = command[0];
+            }
+
+            
+            Console.Write(string.Join(", ", initialList));
+
+
+        }
+    }
+}
