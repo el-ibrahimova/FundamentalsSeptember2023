30a47da1d98312100e1eb3f09984a331eb76ac3d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-17 20:18:01 +0300

    update

--- MidExamPreparation/01.GuineaPig/Program.cs
+++ MidExamPreparation/01.GuineaPig/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double food = double.Parse(Console.ReadLine()) * 1000;
+            double hay = double.Parse(Console.ReadLine()) * 1000;
+            double cover = double.Parse(Console.ReadLine()) * 1000;
+            double guineasKg = double.Parse(Console.ReadLine()) * 1000;
+
+            for (int day  = 1; day  <= 30; day ++)
+            {
+                food -= 300;
+
+                // ecery second day
+                if (day % 2 == 0)
+                {
+                    double hayAmount = food * 0.05;
+                    hay -= hayAmount;
+                }
+
+                // every third day
+                if (day % 3 == 0)
+                {
+                    double coverAmount = guineasKg / 3;
+                    cover -= coverAmount;
+                
+                }
+
+                if (food <= 0 || hay <= 0 || cover <= 0)
+                {
+                    Console.WriteLine("Merry must go to the pet store!");
+                    return;
+                }
+            }
+            Console.WriteLine($"Everything is fine! Puppy is happy! Food: {(food/1000):f2}, Hay: {(hay/1000):f2}, Cover: {(cover/1000):f2}.");
+
+
         }
     }
 }
