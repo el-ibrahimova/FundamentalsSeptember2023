8ea7ce56660771f99a71010e9fcdc53c0de32656
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-20 21:45:30 +0300

    create project

---
+++ MidExamPreparation/01.BonusScoringSystem/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int students = int.Parse(Console.ReadLine());
+        int lectures = int.Parse(Console.ReadLine());
+        int additionalBonus = int.Parse(Console.ReadLine());
+
+        double totalBonus;
+        double maxBonus = int.MinValue;
+        double studentMaxAttendance = 0;
+
+        for (int i = 1; i < students; i++)
+        {
+            double attendance = double.Parse(Console.ReadLine());
+
+            totalBonus = attendance / lectures * (5 + additionalBonus);
+
+            if (totalBonus > maxBonus)
+            {
+                maxBonus = totalBonus;
+                studentMaxAttendance = attendance;
+            }
+        }
+
+        if (true)
+        {
+            Console.WriteLine($"Max Bonus: {Math.Round(maxBonus)}.");
+            Console.WriteLine($"The student has attended {studentMaxAttendance} lectures.");
+
+        }
+
+
+
+    }
+}
