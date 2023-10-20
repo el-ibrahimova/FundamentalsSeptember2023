8f7c6966b41ddb622661066e70af026b8c762eed
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-20 22:52:47 +0300

    update

--- MidExamPreparation/01.BonusScoringSystem/Program.cs
+++ MidExamPreparation/01.BonusScoringSystem/Program.cs
@@ -3,18 +3,25 @@
     private static void Main(string[] args)
     {
         int students = int.Parse(Console.ReadLine());
-        int lectures = int.Parse(Console.ReadLine());
+        double lectures = int.Parse(Console.ReadLine());
         int additionalBonus = int.Parse(Console.ReadLine());
 
-        double totalBonus;
+        double totalBonus =0;
         double maxBonus = int.MinValue;
         double studentMaxAttendance = 0;
 
-        for (int i = 1; i < students; i++)
+        if (students == 0 || lectures == 0)
+        {
+            Console.WriteLine($"Max Bonus: 0.");
+            Console.WriteLine($"The student has attended 0 lectures.");
+            return;
+        }
+
+        for (int i = 1; i <= students; i++)
         {
             double attendance = double.Parse(Console.ReadLine());
 
-            totalBonus = attendance / lectures * (5 + additionalBonus);
+            totalBonus = (double)attendance / lectures * (5 + additionalBonus);
 
             if (totalBonus > maxBonus)
             {
@@ -25,7 +32,7 @@
 
         if (true)
         {
-            Console.WriteLine($"Max Bonus: {Math.Round(maxBonus)}.");
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(maxBonus)}.");
             Console.WriteLine($"The student has attended {studentMaxAttendance} lectures.");
 
         }
--- MidExamPreparation/02.MuOnline/Program.cs
+++ MidExamPreparation/02.MuOnline/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> dungeonRooms = Console.ReadLine()
+             .Split("|", StringSplitOptions.RemoveEmptyEntries)
+             .ToList();
+
+       
+
+        int health = 100;
+        int bitcoin = 0;
+
+        for (int i = 0; i < dungeonRooms.Count; i++)
+        {
+            List<string> anyRoom = new List<string>() { dungeonRooms[i] }; // rat 10
+
+            string command = anyRoom[0];
+            string number = anyRoom[1];
+
+
+            if (command == "potion")
+            {
+                if (health < 100 )
+                {
+                    health += 0;
+                }
+                else
+                {
+                    health = 100;
+                }
+            }
+            else if (command == "chest")
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
