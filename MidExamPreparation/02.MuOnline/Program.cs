822d57ab60bf27769017ddb29be671bc0a3fcb14
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-20 23:31:06 +0300

    update

--- MidExamPreparation/02.MuOnline/Program.cs
+++ MidExamPreparation/02.MuOnline/Program.cs
@@ -6,37 +6,77 @@
              .Split("|", StringSplitOptions.RemoveEmptyEntries)
              .ToList();
 
-       
+        bool isFinished = false;
+        bool isMaxBitcoin = false;
 
         int health = 100;
         int bitcoin = 0;
+        int maxBitCoin = 0;
 
         for (int i = 0; i < dungeonRooms.Count; i++)
         {
-            List<string> anyRoom = new List<string>() { dungeonRooms[i] }; // rat 10
+            string room = dungeonRooms[i];
+            string[] splitted = room.Split();
 
-            string command = anyRoom[0];
-            string number = anyRoom[1];
+            string command = splitted[0];
+            int value = int.Parse(splitted[1]);
 
+            if (maxBitCoin > bitcoin)
+            {
+                maxBitCoin = bitcoin;
+                isMaxBitcoin = true;
+
+            }
 
             if (command == "potion")
             {
-                if (health < 100 )
+                if (health < 100)
                 {
-                    health += 0;
+                    health += value;
+                    if (health > 100)
+                    {
+                        value = health - 100;
+                        health = 100;
+                        
+                    }
                 }
                 else
                 {
                     health = 100;
                 }
+                Console.WriteLine($"You healed for {value} hp.");
+                Console.WriteLine($"Current health: {health} hp.");
+
             }
             else if (command == "chest")
             {
-
+                bitcoin += value;
+                Console.WriteLine($"You found {value} bitcoins.");
             }
             else
             {
+                health -= value;
+                if (health > 0)
+                {
+                    Console.WriteLine($"You slayed {command}.");
+                }
+                else
+                {
+                    Console.WriteLine($"You died! Killed by {command}.");
+                    if (true)
+                    { isMaxBitcoin = true;
+                        Console.WriteLine($"Best room: {i}");
+                    }
+                    
+                    break;
+                }
+            }
 
+            if (isFinished == true)
+            {
+                Console.WriteLine($"You've made it!");
+                Console.WriteLine($"Bitcoins: {bitcoin}");
+                Console.WriteLine($"Health: {health}");
             }
         }
     }
