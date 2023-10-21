19d769395d8cb3777ea6903f5098d6a3bdd06892
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-21 14:16:25 +0300

    create project

--- MidExamPreparation/02.TreasureHunt/Program.cs
+++ MidExamPreparation/02.TreasureHunt/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace _02.TreasureHunt
+﻿using System.ComponentModel.Design;
+
+namespace _02.TreasureHunt
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> treasureChest = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> commands = Console.ReadLine().Split().ToList();
+
+            string input = commands[0];
+            while (input != "Yohoho!")
+            {
+
+                if (input == "Loot")
+                {
+
+                }
+                else if (input == "Drop")
+                {
+
+                }
+                else if (input == "Steal")
+                { 
+                
+                }
+
+                commands = Console.ReadLine().Split().ToList();
+                input = commands[0];
+            }
         }
     }
 }
