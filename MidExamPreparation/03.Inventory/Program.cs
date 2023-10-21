0fc8ec189d4299ad881bcd0d8ed3acb3c900488f
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-21 11:38:06 +0300

    update

--- MidExamPreparation/03.Inventory/Program.cs
+++ MidExamPreparation/03.Inventory/Program.cs
@@ -6,28 +6,19 @@ namespace _03.Inventory
     {
         static void Main(string[] args)
         {
-            List<string> journal = Console.ReadLine()
-                .Split(", ")
-                .ToList();
-
+            List<string> journal = Console.ReadLine().Split(", ").ToList();
             List<string> commands = Console.ReadLine().Split(" - ").ToList();
 
             string input = commands[0];
             while (input != "Craft!")
-            {
-               
+            {  
                 if (input == "Collect")
                 {
                     string item = commands[1];
-                    if (journal.Contains(item))
+                    if (!journal.Contains(item))
                     {
-                        continue;
+                     journal.Add(item);
                     }
-                    else
-                    { 
-                    journal.Add(item);
-                    }
-
                 }
                 else if (input == "Drop")
                 {
@@ -36,7 +27,6 @@ namespace _03.Inventory
                     {
                          journal.Remove(item);
                     }
-                   
                 }
                 else if (input == "Combine Items")
                 {
@@ -44,13 +34,11 @@ namespace _03.Inventory
                     string oldI = item[0];
                     string newI = item[1];
 
-
                     if (journal.Contains(oldI))
                     {
                         int indexOfOldItem = journal.IndexOf(oldI);
                         journal.Insert(indexOfOldItem + 1, newI);
                     }
-                    
                 }
                 else if (input == "Renew")
                 {
@@ -60,7 +48,6 @@ namespace _03.Inventory
                         journal.Remove(item);
                         journal.Add(item);
                     }
-
                 }
 
                 commands = Console.ReadLine().Split(" - ").ToList();
