c7b1a3ac4243f6ec62f787817aae305486168b94
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-22 19:21:21 +0300

    create project

---
+++ MidExamPreparation/Exam.01.CookingMasterclass/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exam._01.CookingMasterclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double flourPrice = double.Parse(Console.ReadLine());
+            double eggPrice = double.Parse(Console.ReadLine());
+            double apronPrice = double.Parse(Console.ReadLine());
+
+            double totalCost = 0;
+            int freePackage = 0;
+          
+            for (int i = 1; i <= students; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    freePackage++;
+                }
+            }
+            apronPrice = apronPrice* students * 1.2;
+            eggPrice = eggPrice * 10 * (students);
+            flourPrice = flourPrice * (students - freePackage);
+
+                totalCost =Math.Ceiling(apronPrice) + eggPrice + flourPrice;
+
+            if (budget >= totalCost )
+            {
+                Console.WriteLine($"Items purchased for {totalCost:f2}$.");
+            }
+            else 
+            {
+                Console.WriteLine($"{totalCost-budget:f2}$ more needed.");
+            }
+        }
+    }
+}
