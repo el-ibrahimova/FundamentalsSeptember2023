24723338c8b6cc4f6e0432654cf47edc7b2c19f3
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-22 19:23:13 +0300

    create project

---
+++ MidExamPreparation/Exam.02.Numbers/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Exam._02.Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string[] input = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string command = input[0];
+
+            while (command != "Finish")
+            {
+                int value = int.Parse(input[1]);
+
+                if (command == "Add")
+                {
+                    numbers.Add(value);
+                }
+                else if (command == "Remove")
+                {
+                    if (numbers.Contains(value))
+                    {
+                        numbers.Remove(value);
+                    }
+                }
+                else if (command == "Replace")
+                {
+                    int replacement = int.Parse(input[2]);
+
+                    if (numbers.Contains(value))
+                    {
+                        int index = numbers.IndexOf(value);
+                        numbers.RemoveAt(index);
+                        numbers.Insert(index, replacement);
+                    }
+                }
+                else if (command == "Collapse")
+                {
+                    numbers.RemoveAll(x => x < value);
+                }
+
+                input = Console.ReadLine().Split().ToArray();
+                command = input[0];
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
---
+++ MidExamPreparation/Exam.03.ChatLogger/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Exam._03.ChatLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> messages = Console.ReadLine().Split().ToList();
+            List<string> conversation = new List<string>();
+
+            string command = messages[0];
+
+            while (command != "end")
+
+            {
+                string message = messages[1];
+
+                if (command == "Chat")
+                {
+                    conversation.Add(message);
+
+                }
+                else if (command == "Delete")
+                {
+                    if (conversation.Contains(message))
+                    {
+                        conversation.Remove(message);
+                    }
+                }
+                else if (command == "Edit")
+                {
+                    string editedVersion = messages[2];
+
+                    if (conversation.Contains(message))
+                    {
+                        int index = conversation.IndexOf(message);
+                        conversation.Remove(message);
+                        conversation.Insert(index, editedVersion);
+                    }
+                }
+                else if (command == "Pin")
+                {
+                    if (conversation.Contains(message))
+                    {
+                        int index = conversation.IndexOf(message);
+                        conversation.Add(message);
+                        conversation.RemoveAt(index);
+                    }
+
+                }
+                else if (command == "Spam")
+                {
+
+                    for (int i = 1; i < messages.Count; i++)
+                    {
+                        conversation.Add(messages[i]);
+                    }
+                }
+
+                messages = Console.ReadLine().Split().ToList();
+                command = messages[0];
+            }
+
+            for (int i = 0; i < conversation.Count; i++)
+            {
+                Console.WriteLine(conversation[i]);
+            }
+        }
+    }
+}
