3d0198507d1e2d8432ee49d21a581cb463719dca
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-25 22:31:56 +0300

    create project

---
+++ 06.ObjectsAndClassess/01.RandomizeWords/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
