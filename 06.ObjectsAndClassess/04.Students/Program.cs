7466d387389869395b80567cc3bbe5a057c25fbf
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-27 20:20:37 +0300

    update

--- 06.ObjectsAndClassess/04.Students/Program.cs
+++ 06.ObjectsAndClassess/04.Students/Program.cs
@@ -6,5 +6,21 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+    }
+
+
+    public class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        { 
+        
+        }
+            
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }    
+         public int Age { get; set; }
+
+        public string HomeTown { get; set; }
     }
 }
