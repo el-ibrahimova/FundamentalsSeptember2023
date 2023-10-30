89edd6acb85464cdb8630f4082aa7ee3e9858eb8
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-10-30 22:50:29 +0200

    create project

---
+++ 06.ObjectsAndClasses-MoreExercise/03.SpeedRacing/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _03.SpeedRacing
+{
+
+    class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsPerKm { get; set; }
+        public double TraveledDistance { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersOfCars = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+
+
+            for (int i = 0; i < numbersOfCars; i++)
+            {
+                string[] carInfo = Console.ReadLine().Split();
+
+                Car car = new Car();
+
+                car.Model = carInfo[0];
+                car.FuelAmount = double.Parse(carInfo[1]);
+                car.FuelConsPerKm = double.Parse(carInfo[2]);
+                car.TraveledDistance = 0;
+
+
+
+
+            }
+        }
+    }
+}
