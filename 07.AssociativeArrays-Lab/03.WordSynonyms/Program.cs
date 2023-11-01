2397cfb3dd64cae4a154b606a7b7e0f3c1a1bc75
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-01 22:35:59 +0200

    create project

---
+++ 07.AssociativeArrays-Lab/03.WordSynonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string key = Console.ReadLine();
+                string value = Console.ReadLine();
+
+                if (!words.ContainsKey(key))
+                {
+                    List<string> newWords = new List<string>();
+                    words.Add(key, newWords);
+                }
+
+                List<string> currentWords = words[key];
+                currentWords.Add(value);
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+        
+    }
+}
