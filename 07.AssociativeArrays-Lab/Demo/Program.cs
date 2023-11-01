02236e54ad5c63eca0914be62aea794950e47cec
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-01 21:08:02 +0200

    create project

---
+++ 07.AssociativeArrays-Lab/Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+
+            if (!phoneNumbers.ContainsKey("stosho"))
+            {
+                phoneNumbers.Add("stosho", "+35988888");
+                }
+
+            if (phoneNumbers.ContainsKey("stosho"))
+            {
+                string phoneNumber = phoneNumbers["stosho"];
+                Console.WriteLine(phoneNumber);
+            }
+
+            bool isDeleted = phoneNumbers.Remove("stosho");
+            Console.WriteLine(isDeleted);
+        }
+    }
+}
