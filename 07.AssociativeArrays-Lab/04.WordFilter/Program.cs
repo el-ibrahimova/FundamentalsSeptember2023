58e7a53e11ceea26e233b4ceb1eac1fbbfdcebed
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-02 21:45:49 +0200

    create project

---
+++ 07.AssociativeArrays-Lab/04.WordFilter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+               .Split()
+                .Where(x => x.Length % 2 == 0)
+                .ToList();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+           
+            // може да се запише и по-кратко
+
+            //     Console.ReadLine()
+            //   .Split()
+            //   .Where(x => x.Length % 2 == 0)
+            //   .ToList()
+            //   .ForEach(Console.WriteLine) ;
+
+
+        }
+    }
+}
