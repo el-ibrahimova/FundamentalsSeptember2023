9cd1845f05912405ea548fd712d50d27f7201270
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-03 20:31:19 +0200

    update

--- 07.AssociativeArrays-Exercise/01.CountCharsInAString/Program.cs
+++ 07.AssociativeArrays-Exercise/01.CountCharsInAString/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> charOccurances = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char character = input[i];
+
+                if (character == ' ')
+                {
+                    continue;
+                }
+
+                if (!charOccurances.ContainsKey(character))
+                {
+                    charOccurances.Add(character, 0);
+                }
+                charOccurances[character]++;
+
+            }
+
+            foreach (var occurance in charOccurances)
+            {
+                char symbol = occurance.Key;
+                int count = occurance.Value;
+
+                Console.WriteLine($"{symbol} -> {count}");
+            }
         }
     }
 }
