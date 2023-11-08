9e9ecc7bb913c6023a5935d6a8e117eac66d1bfc
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-08 20:20:20 +0200

    create project

---
+++ 08.TextProcessing-Lab/03.Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string words = Console.ReadLine();
+
+            while (words.Contains(wordToRemove))
+            {
+                int index = words.IndexOf(wordToRemove);
+
+               words = words.Remove(index, wordToRemove.Length); // презаписваме думата
+            }
+            Console.WriteLine(words);
+
+        }
+    }
+}
