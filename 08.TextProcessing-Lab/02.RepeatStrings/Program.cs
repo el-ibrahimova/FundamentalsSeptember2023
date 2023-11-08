d6c00c9ee2b40f2f81a89677e6404d81b6494fad
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-08 19:42:06 +0200

    create project

---
+++ 08.TextProcessing-Lab/02.RepeatStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            string result = string.Empty;
+
+            foreach (string word in words)
+            {
+                int length = word.Length; // hi -> 2
+
+                for (int i = 0; i < length; i++)
+                {
+                    result += word;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
