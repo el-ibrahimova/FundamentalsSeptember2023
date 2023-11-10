01627fdc1608cc60dd28bc6257dba9cda8a79285
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-10 21:35:30 +0200

    update

--- 08.TextProcessing-Exercise/04.CaesarCipher/Program.cs
+++ 08.TextProcessing-Exercise/04.CaesarCipher/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace _04.CaesarCipher
+﻿using System.Data.Common;
+using System.Text;
+
+namespace _04.CaesarCipher
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = Console.ReadLine();
+
+            StringBuilder cipherBuilder = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char original = text[i];
+                cipherBuilder.Append((char)(original+3)); // ако остане само original +3  резултата ще е число
+            }
+            Console.WriteLine(cipherBuilder);
         }
     }
 }
