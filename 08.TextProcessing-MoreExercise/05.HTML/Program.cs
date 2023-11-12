82ab67bf746c6b81768e1cf450541bc786f8cc8e
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-12 13:45:13 +0200

    create project

---
+++ 08.TextProcessing-MoreExercise/05.HTML/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _05.HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+
+            List<string> comments = new List<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "end of comments")
+            {
+                comments.Add(input);
+            }
+
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"\t {title}");
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");
+            Console.WriteLine($"\t {content}");
+            Console.WriteLine("</article>");
+
+           
+            foreach (string comment in comments)
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine($"\t {comment}");
+                Console.WriteLine("</div>");
+            }
+
+        }
+    }
+}
