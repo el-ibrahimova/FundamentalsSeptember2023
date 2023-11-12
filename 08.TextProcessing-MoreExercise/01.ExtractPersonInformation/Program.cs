44f705d5d2d18dc84c615eae40b60493c471fa27
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-12 13:42:28 +0200

    create project

---
+++ 08.TextProcessing-MoreExercise/01.ExtractPersonInformation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace _01.ExtractPersonInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+
+                int nameStartIndex = (input.IndexOf('@')) + 1;
+                int nameEndIndex = input.IndexOf('|');
+                int nameLength = nameEndIndex - nameStartIndex;
+
+                string name = input.Substring(nameStartIndex, nameLength);
+
+                int ageStartIndex = (input.IndexOf('#')) + 1;
+                int ageEndIndex = input.IndexOf('*');
+                int ageLength = ageEndIndex - ageStartIndex;
+
+                string age = input.Substring(ageStartIndex, ageLength);
+
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
