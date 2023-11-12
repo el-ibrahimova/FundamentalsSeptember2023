acf85896dcf6fc9936cc285c4a2712fab8e053ed
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-12 13:43:11 +0200

    create project

---
+++ 08.TextProcessing-MoreExercise/02.AsciiSumator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.AsciiSumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            string random = Console.ReadLine();
+
+            int firstIndex = (int)first;
+            int secondIndex = (int)second;
+            int sum = 0;
+
+
+            foreach (char symbol in random)
+            {
+                if (symbol > firstIndex && symbol < secondIndex)
+                {
+                    sum += symbol;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
