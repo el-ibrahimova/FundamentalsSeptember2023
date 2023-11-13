2176fb1ac258f03662d82f14ed3a121968710a3d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-13 19:27:24 +0200

    update

--- 06.ObjectsAndClassess/03.Songs/Program.cs
+++ 06.ObjectsAndClassess/03.Songs/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
 
 namespace _03.Songs
 {
@@ -11,7 +12,7 @@ namespace _03.Songs
 
             List<Song> songs = new List<Song>();
 
-            for (int i = 0; i <n ; i++)
+            for (int i = 0; i < n; i++)
             {
                 string[] currentInput = Console.ReadLine().Split("_");
 
@@ -22,37 +23,56 @@ namespace _03.Songs
                 Song song = new Song(typeList, name, time); // създаваме обекта 
                 songs.Add(song);
             }
-           
-            string list = Console.ReadLine(); // "all" или TypeList - по условие
-         
-            for (int i = 0; i < songs.Count; i++)
-            { 
-                Song currentSong = (songs[i]);
 
-                if (list == "all")
+            string list = Console.ReadLine(); // "all" или TypeList - по условие
+
+
+            if (list == "all")
+            {
+                foreach (Song song in songs)
                 {
-                    Console.WriteLine(currentSong.Name);
-                }
-                else if (list == currentSong.TypeList)
-                {
-                    Console.WriteLine(currentSong.Name);
+                    Console.WriteLine(song.Name);
                 }
             }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList == list)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+
+            // записване на output-a с LINQ
+            //  if (list == "all")
+            //  {
+            //     var searchSongs = songs.Select(x => x.Name);
+            //     Console.WriteLine(string.Join("", searchSongs));
+            //  }
+            //  else 
+            // {
+            //      var searchSongs = songs.Where(x=>x.TypeList == list).Select(x => x.Name);
+            //      Console.WriteLine(string.Join("", searchSongs));
+            // } 
+
         }
-    }
-
-    public class Song
-    {
-        public Song(string typeList, string name, string time)
-        {
-            TypeList = typeList;
-            Name = name;
-            Time = time;
-        }
-
-        public string TypeList { get; set; }
-
-        public string Name { get; set; }
-        public string Time { get; set; }
     }
 }
+
+public class Song
+{
+    public Song(string typeList, string name, string time)
+    {
+        TypeList = typeList;
+        Name = name;
+        Time = time;
+    }
+
+    public string TypeList { get; set; }
+
+    public string Name { get; set; }
+    public string Time { get; set; }
+}
+
