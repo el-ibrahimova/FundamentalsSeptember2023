5d388f371ed5f59366263b346aa471bfda65837e
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-13 22:23:38 +0200

    Update

--- 08.TextProcessing-MoreExercise/05.HTML/Program.cs
+++ 08.TextProcessing-MoreExercise/05.HTML/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace _05.HTML
+﻿using System.Text;
+
+namespace _05.HTML
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
             string title = Console.ReadLine();
+            string content = Console.ReadLine();
+
+            StringBuilder htmlStyle = new StringBuilder();
+            htmlStyle.AppendLine("<h1>");
+            htmlStyle.AppendLine("\t" + title);
+            htmlStyle.AppendLine("</h1>");
+            htmlStyle.AppendLine("<article>");
+            htmlStyle.AppendLine("\t" + content);
+            htmlStyle.AppendLine("</article>");
+
+            string comment;
+            while ((comment = Console.ReadLine()) != "end of comments")
+            {
+                htmlStyle.AppendLine("<div>");
+                htmlStyle.AppendLine("\t" + comment);
+                htmlStyle.AppendLine("</div>");
+            }
+
+            Console.WriteLine(htmlStyle);
+
+
+
+            /*  друго решение
+             *  
+               string title = Console.ReadLine();
             string content = Console.ReadLine();
 
             List<string> comments = new List<string>();
@@ -29,6 +57,7 @@
                 Console.WriteLine("</div>");
             }
 
+             */
         }
     }
 }
