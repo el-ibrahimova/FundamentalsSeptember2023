547a7dd7cb98719b25099891a9f9c9cca81965cb
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-14 20:21:18 +0200

    update

--- 09.RegularExpressions-Lab/01.MatchFullName/Program.cs
+++ 09.RegularExpressions-Lab/01.MatchFullName/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace _01.MatchFullName
+﻿using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchCollection = regex.Matches(input);
+
+            Console.WriteLine(string.Join(" ", matchCollection));
         }
     }
 }
