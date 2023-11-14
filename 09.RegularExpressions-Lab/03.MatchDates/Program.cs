a7ebbe454681a5d8e4b99fb50b339adc7a1396c6
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-14 21:19:11 +0200

    create project

---
+++ 09.RegularExpressions-Lab/03.MatchDates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<day>\d{2})(\.|-|/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})";
+
+            MatchCollection mathCollection = Regex.Matches(input, pattern);
+
+            foreach (Match match in mathCollection)
+            {
+                var groups = match.Groups;
+
+                Console.WriteLine($"Day: {groups["day"]}, Month: {groups["month"]}, Year: {groups["year"]}");
+            }
+        }
+    }
+}
