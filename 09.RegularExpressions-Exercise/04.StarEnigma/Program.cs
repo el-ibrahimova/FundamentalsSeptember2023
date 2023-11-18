653317c407860a25e297e3eef5ec0f0b1ea2b69e
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-18 22:14:48 +0200

    update

--- 09.RegularExpressions-Exercise/04.StarEnigma/Program.cs
+++ 09.RegularExpressions-Exercise/04.StarEnigma/Program.cs
@@ -1,34 +1,30 @@
 ﻿using System.Text.RegularExpressions;
 using System.Text;
+using System.Threading.Tasks.Dataflow;
 
 namespace _04.StarEnigma
 {
     public class Message
     {
-        public Message(string planetName, uint population, string attackType, uint soldierCount)
-        {
-            PlanetName = planetName;
-            Population = population;
-            AttackType = attackType;
-            SoldierCount = soldierCount;
-        }
         public string PlanetName { get; set; }
         public uint Population { get; set; }
         public string AttackType { get; set; }
         public uint SoldierCount { get; set; }
     }
-    
-    
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Message> messages = new List<Message>();
+
             int messageCount = int.Parse(Console.ReadLine());
-            
+
             string starPattern = @"[SsTtAaRr]";
 
             string messagePattern = @"@(?<Planet>[A-Za-z]+)[^\@\-\!\:\>]*:(?<Population>\d+)[^\@\-\!\:\>]*\!(?<AtackType>A|D)\![^\@\-\!\:\>]*\-\>(?<SoldierCount>\d+)";
-            
+
             for (int i = 0; i < messageCount; i++)
             {
                 string encryptMessage = Console.ReadLine();
@@ -38,11 +34,41 @@ namespace _04.StarEnigma
 
                 for (int j = 0; j < encryptMessage.Length; j++)
                 {
-                   messageBuilder.Append((char)(encryptMessage[j] - decryptionKey));
+                    messageBuilder.Append((char)(encryptMessage[j] - decryptionKey));
                 }
-                
+
+                string decryptedMessage = messageBuilder.ToString();
+
+                Match match = Regex.Match(decryptedMessage, messagePattern);
+
+                if (match == null) // проверяваме дали има съвпадения
+                {
+                    continue;      // ако няма, то продължаваме със следващия ред
+                }
+
+                Message message = new Message();
+
+                message.PlanetName = match.Groups["Planet"].Value;
+                message.Population = uint.Parse(match.Groups["Population"].Value);
+                message.AttackType = match.Groups["AtackType"].Value;
+                message.SoldierCount = uint.Parse(match.Groups["SoldierCount"].Value);
+
+                messages.Add(message);
             }
-            
+
+            var planets = messages
+                .Where(m => m.AttackType == "A")
+                .OrderBy(m => m.PlanetName)
+                .ToList();
+            Console.WriteLine($"Attacked planets:{planets.Count}");
+            planets.ForEach(m => Console.WriteLine($"-> {m.PlanetName}"));
+
+            // горното е кратък запис на това:
+            //  foreach (Message message in planets)
+            //  {
+            //     Console.WriteLine($"-> {message.PlanetName}");
+            // }
+
         }
     }
 }
