458d828fc71c625b57ea55ed55f4bc3de3ae902a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-22 20:35:01 +0200

    create project

---
+++ ProgrammingFundamentalsFinalExamPreparation/01.SecretChat/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.SecretChat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretMessage = Console.ReadLine();
+
+            string line;
+            while ((line = Console.ReadLine()) != "Reveal")
+            { 
+            
+            }
+        }
+    }
+}
