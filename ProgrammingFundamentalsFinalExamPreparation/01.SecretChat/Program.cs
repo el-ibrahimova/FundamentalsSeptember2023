e75637f382411bbafa59809ac067f905196fa6d6
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-22 20:43:06 +0200

    update

--- ProgrammingFundamentalsFinalExamPreparation/01.SecretChat/Program.cs
+++ ProgrammingFundamentalsFinalExamPreparation/01.SecretChat/Program.cs
@@ -8,8 +8,29 @@
 
             string line;
             while ((line = Console.ReadLine()) != "Reveal")
-            { 
-            
+            {
+                string[] tokens = line.Split(":|:");
+
+                string command = tokens[0];
+
+                if (command == "InsertSpace")
+                {
+                    int index = int.Parse(tokens[1]);
+                    secretMessage = secretMessage.Insert(index, " ");
+                    Console.WriteLine(secretMessage);
+                }
+                else if (command == "ChangeAll")
+                {
+                    string substring = tokens[1];
+                    string replacement = tokens[2];
+
+                    secretMessage = secretMessage.Replace(substring, replacement);
+                }
+                else if (command == "Reverse")
+                { 
+                
+                }
+
             }
         }
     }
