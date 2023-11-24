8db5657e5334d0d88cc372ea9f46690a60502cf9
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-24 20:58:19 +0200

    update

--- ProgrammingFundamentalsFinalExamPreparation/01.ActivationKeys-Exam/Program.cs
+++ ProgrammingFundamentalsFinalExamPreparation/01.ActivationKeys-Exam/Program.cs
@@ -2,9 +2,55 @@
 {
     internal class Program
     {
+        static string activationKey;
+        // пищшем го като пропърти на класа Program, за да е видимо във всички методи (за да не се повтаря като променлива и аргумент в методите на класа)
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            activationKey = Console.ReadLine();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Generate")
+            {
+                string[] arguments = input.Split(">>>");
+                switch (arguments[0])
+                {
+                    case "Contains":
+                        string substring = arguments[1];
+                        // така трябваше да го запишем, ако не бяхме изнесли activationKey като пропърти на класа Program
+                        // activationKey = Contains(activationKey, substring);
+
+                        Contains(substring);
+                        break;
+
+                    case "Flip":
+                        if (arguments[1] == "Upper")
+                        {
+
+                        }
+                        else if (arguments[1] == "Lower")
+                        { 
+                        
+                        }
+                        break;
+
+
+
+                    case "Slice": break;
+                }
+            }
+        }
+
+        static void Contains(string substring)
+        {
+            if (activationKey.Contains(substring))
+            {
+                Console.WriteLine($"{activationKey} contains {substring}");
+            }
+            else
+            {
+                Console.WriteLine($"Substring not found!");
+            }
         }
     }
 }
