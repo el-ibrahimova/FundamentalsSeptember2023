05f5b3036e457a2546699e23aa4717eddd5fdd42
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-11-26 10:35:04 +0200

    remove project

---
+++ Demo/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> res = new Dictionary<string, int>();
+
+      
+            while (true)
+            {
+                      string input = Console.ReadLine();
+                
+                if (input == "stop")
+                {
+                    break;
+                }
+
+               
+
+
+                if (!res.ContainsKey(input))
+                {
+                    res.Add(input, 0);
+                }
+                
+                int quantity =int.Parse(Console.ReadLine());
+                res[input] += quantity;
+                
+            }
+
+            foreach (var some in res)
+            {
+                Console.WriteLine($"{some.Key} -> {some.Value}");
+            }
+
+        }
+    }
+}
