940628c18125fe5f0440747bda182c794601e2de
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-01 11:17:08 +0200

    create project

---
+++ ProgrammingFundamentalsFinalExamPreparation/02.DestinationMapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    internal class Program
+    {     
+        static void Main(string[] args)
+        {  
+            List<string> destinations = new List<string>();
+            string text = Console.ReadLine();
+
+            string pattern = @"(=|\/)(?<destination>[A-Z][A-Za-z]{2,})\1";
+
+            foreach (Match match in Regex.Matches(text, pattern))
+            {
+                string destination = match.Groups["destination"].Value;
+                destinations.Add(destination);
+            }
+
+            int points = 0;
+            foreach (var dest in destinations)
+            {
+                points += dest.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {points}");
+        }
+    }
+}
