cd4b3cee55315b8391a77e0391a301f47714aa20
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-02 11:01:45 +0200

    create project

---
+++ ProgrammingFundamentalsFinalExamPreparation/03.Santa'sNewList/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _03.Santa_sNewList
+{   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // name, points
+            Dictionary<string, int> childrens = new Dictionary<string, int>();
+
+            //present, points
+            Dictionary<string, int> presents = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] info = input.Split("->");
+
+                if (info[0] != "Remove")
+                {
+                    string name = info[0];
+                    int amount = int.Parse(info[2]);
+                    string present = info[1];
+
+                    if (!childrens.ContainsKey(name) ) 
+                    {
+                        childrens.Add(name, amount);                    
+                    }
+                    else
+                    {
+                        childrens[name] += amount;                        
+                    }
+                   
+                    if (!presents.ContainsKey(present))
+                    {                       
+                        presents.Add(present, amount);
+                    }
+                    else
+                    {                
+                        presents[present] += amount;
+                    }
+                }
+                else
+                {
+                    string childName = info[1];
+                    childrens.Remove(childName);
+                }
+            }
+
+            Console.WriteLine("Children:");
+            foreach (var ch in childrens.OrderByDescending(t => t.Value).ThenBy(n => n.Key))
+            {
+                Console.WriteLine($"{ch.Key} -> {ch.Value}");
+            }
+
+            Console.WriteLine("Presents:");
+            foreach (var ch in presents)
+            {
+                Console.WriteLine($"{ch.Key} -> {ch.Value}");
+            }
+        }
+    }
+}
