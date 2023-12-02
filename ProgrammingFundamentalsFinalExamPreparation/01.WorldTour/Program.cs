a0231728b42f1098cf65f78e682170b275a0bf90
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-02 10:59:53 +0200

    create project

---
+++ ProgrammingFundamentalsFinalExamPreparation/01.WorldTour/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _01.WorldTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string input = Console.ReadLine();
+
+            string commands;
+            while ((commands = Console.ReadLine()) != "Travel")
+            {
+                string[] command = commands.Split(":");
+                string operation = command[0];
+
+                if (operation == "Add Stop")
+                {
+                    int index = int.Parse(command[1]);
+                    string text = command[2];
+
+                    if (index >= 0 && index <= input.Length)
+                    {
+                        input = input.Insert(index, text);
+                    }
+                    Console.WriteLine(input);
+                }
+                else if (operation == "Remove Stop")
+                {
+                    int startIndex = int.Parse(command[1]);
+                    int endIndex = int.Parse(command[2]);
+
+                    if (startIndex >= 0 && startIndex <= input.Length-1 
+                        && endIndex >= 0 && endIndex <= input.Length-1)
+                        {
+                            input = input.Remove(startIndex, endIndex+1-startIndex);
+                        }
+                    Console.WriteLine(input);
+                }
+                else // "Switch"
+                {
+                    string oldString = command[1];
+                    string newString = command[2];
+
+                    if (input.Contains(oldString))
+                    {
+                        input = input.Replace(oldString, newString);
+                    }
+                    Console.WriteLine(input);
+                }
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {input}");
+        }
+    }
+}
